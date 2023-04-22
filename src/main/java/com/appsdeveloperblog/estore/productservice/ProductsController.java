987e110a78c1dfc100f4b2fa72a95a36cb1f99e0
dayaoca <dayalanoca8@gmail.com>
2023-04-23 05:02:35 +0530

    section3-commit

---
+++ src/main/java/com/appsdeveloperblog/estore/productservice/ProductsController.java
@@ -0,0 +1,28 @@
+package com.appsdeveloperblog.estore.productservice;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @PostMapping
+    public String createProduct(){
+        return "Http post method";
+    }
+
+    @GetMapping
+    public String getProduct(){
+        return "Http get method";
+    }
+
+    @PutMapping
+    public String updateProduct(){
+        return "Http put method";
+    }
+
+    @DeleteMapping
+    public String deleteProduct(){
+        return "Http delete method";
+    }
+}

d790250f91ff46a051a1fe7a00edd56d15cb875a
thilak <daya.22.thilak@gmail.com>
2023-06-01 09:29:42 +0530

    committing- docker command to run axon server

--- src/main/java/com/appsdeveloperblog/estore/productservice/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/estore/productservice/ProductsController.java
@@ -1,19 +1,24 @@
 package com.appsdeveloperblog.estore.productservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
 
+    @Autowired
+    private Environment env;
+
     @PostMapping
     public String createProduct(){
-        return "Http post method";
+        return "Http post method ";
     }
 
     @GetMapping
     public String getProduct(){
-        return "Http get method";
+        return "Http get method "+env.getProperty("local.server.port");
     }
 
     @PutMapping

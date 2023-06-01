129f904f50ab29ff651baea5fc002a2e1269aa6a
thilak <daya.22.thilak@gmail.com>
2023-06-01 16:18:48 +0530

    step52-validate createproduct command

--- src/main/java/com/appsdeveloperblog/estore/productservice/command/ProductAggregate.java
+++ src/main/java/com/appsdeveloperblog/estore/productservice/command/ProductAggregate.java
@@ -3,6 +3,8 @@ package com.appsdeveloperblog.estore.productservice.command;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import java.math.BigDecimal;
+
 @Aggregate
 public class ProductAggregate {
 
@@ -12,6 +14,12 @@ public class ProductAggregate {
 
     @CommandHandler
     public ProductAggregate(CreateProductCommand createProductCommand){
-
+        //validation logic
+        if(createProductCommand.getPrice().compareTo(BigDecimal.ZERO)<=0){
+            throw new IllegalArgumentException("price cannot be less or equa than zero");
+        }
+        if(createProductCommand.getTitle()==null || createProductCommand.getTitle().isBlank()){
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
     }
 }

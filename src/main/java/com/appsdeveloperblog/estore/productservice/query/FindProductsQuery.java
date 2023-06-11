b2a2333d1fccf232ed055aea6408681fc2482ad2
thilak <daya.22.thilak@gmail.com>
2023-06-11 13:17:36 +0530

    step71,72-added query object, query gateway

---
+++ src/main/java/com/appsdeveloperblog/estore/productservice/query/FindProductsQuery.java
@@ -0,0 +1,4 @@
+package com.appsdeveloperblog.estore.productservice.query;
+
+public class FindProductsQuery {
+}
--- src/main/java/com/appsdeveloperblog/estore/productservice/query/rest/ProductsQueryController.java
+++ src/main/java/com/appsdeveloperblog/estore/productservice/query/rest/ProductsQueryController.java
@@ -1,9 +1,25 @@
 package com.appsdeveloperblog.estore.productservice.query.rest;
 
+import com.appsdeveloperblog.estore.productservice.query.FindProductsQuery;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("")
+@RequestMapping("/products")
 public class ProductsQueryController {
+
+    @Autowired
+    QueryGateway queryGateway;
+
+    public List<ProductRestModel> getProducts(){
+        FindProductsQuery findProductsQuery = new FindProductsQuery();
+        List<ProductRestModel> products = queryGateway.query(findProductsQuery,
+                ResponseTypes.multipleInstancesOf(ProductRestModel.class)).join();
+        return products;
+    }
 }

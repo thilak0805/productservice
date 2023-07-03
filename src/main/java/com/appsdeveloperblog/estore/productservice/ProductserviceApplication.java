a969324d89d6aaa7000eac46e34909d4b496e182
thilak <daya.22.thilak@gmail.com>
2023-07-03 10:46:00 +0530

    step 85-register message dispatch interceptor

--- src/main/java/com/appsdeveloperblog/estore/productservice/ProductserviceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/productservice/ProductserviceApplication.java
@@ -1,8 +1,12 @@
 package com.appsdeveloperblog.estore.productservice;
 
+import com.appsdeveloperblog.estore.productservice.command.interceptor.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
@@ -11,6 +15,11 @@ public class ProductserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductserviceApplication.class, args);
+	}
+
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus){
+		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
 	}
 
 }
